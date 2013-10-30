930cf14bbdf037fdb5f4b5a350c61142130008a1
Leonardo Situmorang <leonardo@situmorang.net>
2013-10-31 00:30:37 +0700

    echo server and a simple client

---
+++ eko.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('engine.io').attach(server);
+
+// see: http://expressjs.com/api.html#app.use
+// ... serving files in ./public using the express.static() middleware
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket){
+  socket.on('message', function(v){
+    // echo
+    socket.send(v);
+  });
+});
+
+server.listen(3000, function(){
+  console.log('listening on localhost:3000');
+});
